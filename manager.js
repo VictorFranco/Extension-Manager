09eb06711fd3d877edd36067ff1e98765094e09f
Victor Franco <vfrancohdez1@hotmail.com>
2020-01-19 21:28:03 -0600

    Se mejoro la legibilidad

--- manager.js
+++ manager.js
@@ -1,68 +1,65 @@
-const contenedor=document.getElementById("extensiones");
-let div=document.createElement("DIV");
+const container=document.getElementById("container");
+const input=document.getElementById("search");
+const all_enabled=document.getElementById("all_enabled");
+const all_disabled=document.getElementById("all_disabled");
+const div=document.createElement("DIV");
 let checks=[];
-let ids=[];
-let checked;
-carga();
-let input=document.getElementById("buscar");
-input.addEventListener("keyup",buscar);
-let habilitar=document.getElementById("habilitar");
-let deshabilitar=document.getElementById("deshabilitar");
-habilitar.addEventListener("click",activar_desactivar);
-deshabilitar.addEventListener("click",activar_desactivar);
-
-function carga(){
+let extensions=[];
+input.addEventListener("keyup",search);
+all_enabled.addEventListener("click",activate_desactivate);
+all_disabled.addEventListener("click",activate_desactivate);
+load();
+function load(){
     chrome.management.getAll(
-        a=>{
-            ids=a;
-            let etiqueta="";
-            ids.map((element,index)=>{
-                etiqueta+="<div class='fila' id='fila"+index+"'>"+icon(element)+name(element.name)+button(index,element.enabled)+"</div>";
-            });
-            contenedor.innerHTML=etiqueta;
-            ids.map((element,index)=>{
-                checks[index]=document.getElementById("check"+index+"");
+        array=>{
+            extensions=array;
+            let html_elements=extensions.map((extension,index)=>{
+                return `<div class="row" id="row${index}">${icon(extension)+name(extension.name)+button(index,extension.enabled)}</div>`;
+            }).reduce((a,b)=>a+b);
+            container.innerHTML=html_elements;
+            extensions.map((element,index)=>{
+                checks[index]=document.getElementById(`check${index}`);
                 checks[index].addEventListener("click",pressed);
             });
     });
 }
-function name(name){
-    return "<div class='div_ext'>"+name+"</div>";
-}
-function icon(id){
-    let etiqueta="";
-    id.icons.map(element=>{if(element.size==128) etiqueta="<img src='"+element.url+"'>";});
-    return etiqueta;
-}
-function button(i,check){
+const name=name=>{
+    return `<div class="div_ext">${name}</div>`;
+};
+const icon=extension=>{
+    let html_element="";
+    extension.icons.map(ext_icon=>{if(ext_icon.size==128) html_element=`<img src="${ext_icon.url}">`;});
+    return html_element;
+};
+const button=(i,check)=>{
     let checked="";
     if(check) checked="checked";
-    return "<input id='check"+i+"' type='checkbox'"+checked+"><label for='check"+i+"'></label>";
-}
+    return `<input id="check${i}" type="checkbox" ${checked}><label for="check${i}"></label>`;
+};
 function pressed(){
     let id=window.event.target.id;
     let num=id.split("check")[1];
     let input=document.getElementById(id);
-    let id_ext=ids[num].id;
+    let id_ext=extensions[num].id;
     chrome.management.setEnabled(id_ext,input.checked);
 }
-function buscar(){
-    ids.map((element,index)=>{    
+function search(){
+    extensions.map((element,index)=>{    
         let display="none";
-        let fila=document.getElementById("fila"+index+"");
+        let row=document.getElementById(`row${index}`);
         let nombre=element.name.toUpperCase();
         if (nombre.indexOf(input.value.toUpperCase())>-1) display=""; 
-        fila.style.display=display;
+        row.style.display=display;
     });
 }
-function activar_desactivar(){
-    let estado=false,elemento;
-    if(window.event.target.id=="habilitar") estado=true;
-    ids.map((element,index)=>{
-        if(chrome.runtime.id!=element.id)
-            chrome.management.setEnabled(element.id,estado,()=>{
-                elemento=document.getElementById("check"+index);
-                elemento.checked=estado;
+function activate_desactivate(){
+    let state=false,toggle;
+    if(window.event.target.id=="all_enabled") state=true;
+    extensions.map((extension,index)=>{
+        if(chrome.runtime.id!=extension.id)
+            chrome.management.setEnabled(extension.id,state,()=>{
+                toggle=document.getElementById(`check${index}`);
+                toggle.checked=state;
             });
     });
 }
