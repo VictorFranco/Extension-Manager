ce2bb9ca39301b4b2e94878e91c175a88c65c71f
Victor Franco <vfrancohdez1@hotmail.com>
2021-07-20 07:52:07 -0500

    Solucionar warnings
    Agregar la interfaz IExtension
    Tipar argumentos de las funciones:
    > name_()
    > icon()
    > button()

--- manager.ts
+++ manager.ts
@@ -10,34 +10,50 @@ input.addEventListener("keyup",search);
 all_enabled.addEventListener("click",activate_desactivate);
 all_disabled.addEventListener("click",activate_desactivate);
 btn_close.addEventListener("click",()=>window.close());
+
 load();
+
 function load(){
     chrome.management.getAll(
         array=>{
             extensions=array;
             let html_elements=extensions.map((extension,index)=>{
-                return `<div class="row" id="row${index}">${icon(extension)+name_(extension.name)+button(index,extension.enabled)}</div>`;
+                return `<div class="row" id="row${index}">${
+                    icon(extension)+
+                    name_(extension.name)+
+                    button(index,extension.enabled)
+                }</div>`;
             }).reduce((a,b)=>a+b);
             container.innerHTML=html_elements;
-            extensions.forEach((element,index)=>{
+            extensions.forEach((_,index)=>{
                 checks[index]=document.getElementById(`check${index}`);
                 checks[index].addEventListener("click",pressed);
             });
     });
 }
-const name_=name=>{
+
+const name_=(name:string)=>{
     return `<div class="div_ext">${name}</div>`;
 };
-const icon=extension=>{
+
+interface IExtension{
+    icons:[{url:string,size:number}]
+}
+
+const icon=(extension:IExtension)=>{
     let html_element="";
-    extension.icons.forEach(ext_icon=>{if(ext_icon.size==128) html_element=`<img src="${ext_icon.url}">`;});
+    extension.icons.forEach(ext_icon=>{
+        if(ext_icon.size==128) html_element=`<img src="${ext_icon.url}">`;
+    });
     return html_element;
 };
-const button=(i,check)=>{
+
+const button=(i:number,check:boolean)=>{
     let checked="";
     if(check) checked="checked";
     return `<input id="check${i}" type="checkbox" ${checked}><label for="check${i}"></label>`;
 };
+
 function pressed(){
     let id=(window.event.target as HTMLInputElement).id;
     let num=id.split("check")[1];
@@ -45,6 +61,7 @@ function pressed(){
     let id_ext=extensions[num].id;
     chrome.management.setEnabled(id_ext,input.checked);
 }
+
 function search(){
     extensions.forEach((element,index)=>{    
         let display="none";
@@ -54,14 +71,15 @@ function search(){
         row.style.display=display;
     });
 }
+
 function activate_desactivate(){
-    let state=false,toggle;
+    let state=false,toggle:HTMLElement;
     if((window.event.target as HTMLButtonElement).id=="all_enabled") state=true;
     extensions.forEach((extension,index)=>{
         if(chrome.runtime.id!=extension.id)
             chrome.management.setEnabled(extension.id,state,()=>{
                 toggle=document.getElementById(`check${index}`);
-                toggle.checked=state;
+                (toggle as HTMLInputElement).checked=state;
             });
     });
 }
